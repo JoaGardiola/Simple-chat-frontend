271eef7159fd89a0d679c9aa8f8ac07739316bcc
JoaGardiola <joa.gardiola@gmail.com>
2020-02-07 11:53:47 -0300

    create signup and signin components

---
+++ src/components/signIn.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button'
+
+export default class Login extends Component {
+  render () {
+    return (
+      <form>
+        <h3>Sign In</h3>
+
+        <div className='form-group'>
+          <label>Username</label>
+          <input type='username' className='form-control' placeholder='Enter username' />
+        </div>
+
+        <div className='form-group'>
+          <label>Password</label>
+          <input type='password' className='form-control' placeholder='Enter password' />
+        </div>
+
+        <div className='form-group'>
+          <div className='custom-control custom-checkbox'>
+            <input type='checkbox' className='custom-control-input' id='customCheck1' />
+            <label className='custom-control-label' htmlFor='customCheck1'>Remember me</label>
+          </div>
+        </div>
+
+        <Button type='submit' className='btn btn-primary btn-block'>Submit</Button>
+        <p className='forgot-password text-right'>
+                  or create an <a href='/auth/signup'>account</a>
+        </p>
+      </form>
+    )
+  }
+}
---
+++ src/components/signUp.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button'
+
+export default class SignUp extends Component {
+  render () {
+    return (
+      <form>
+        <h3>Sign Up</h3>
+
+        <div className='form-group'>
+          <label>First name</label>
+          <input type='text' className='form-control' placeholder='First name' />
+        </div>
+
+        <div className='form-group'>
+          <label>Last name</label>
+          <input type='text' className='form-control' placeholder='Last name' />
+        </div>
+
+        <div className='form-group'>
+          <label>Username</label>
+          <input type='username' className='form-control' placeholder='Enter username' />
+        </div>
+
+        <div className='form-group'>
+          <label>Password</label>
+          <input type='password' className='form-control' placeholder='Enter password' />
+        </div>
+
+        <Button type='submit' className='btn btn-primary btn-block'>Sign Up</Button>
+        <p className='forgot-password text-right'>
+            Already registered? <a href='/auth/signin'>sign in</a>
+        </p>
+      </form>
+    )
+  }
+}
