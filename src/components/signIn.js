ced6d1388573a2d27fbee49806600588c525f390
JoaGardiola <joa.gardiola@gmail.com>
2020-03-09 20:15:51 -0300

    change signIn class component to functional

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -5,33 +5,31 @@ import { Button, Form } from 'react-bootstrap'
 
 import { Link } from 'react-router-dom'
 
-export default class Login extends Component {
-  render () {
-    return (
-      <Form>
-        <h3>Sign In</h3>
+export default function Login () {
+  return (
+    <Form>
+      <h3>Sign In</h3>
 
-        <Form.Group controlId='formUsername'>
-          <Form.Label>Username</Form.Label>
-          <Form.Control type='text' className='form-control' placeholder='Enter username' required />
-        </Form.Group>
+      <Form.Group controlId='formUsername'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control type='text' className='form-control' placeholder='Enter username' required />
+      </Form.Group>
 
-        <Form.Group controlId='formPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type='password' className='form-control' placeholder='Enter password' required />
-        </Form.Group>
+      <Form.Group controlId='formPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type='password' className='form-control' placeholder='Enter password' required />
+      </Form.Group>
 
-        <Form.Group controlId='formCheckbox'>
-          <Form.Check type='checkbox' className={formStyle.custumControlLabel} label='Remember me' />
-        </Form.Group>
+      <Form.Group controlId='formCheckbox'>
+        <Form.Check type='checkbox' className={formStyle.custumControlLabel} label='Remember me' />
+      </Form.Group>
 
-        <Button variant='primary' type='submit' className='btn btn-primary btn-block'>
-          Submit
-        </Button>
-        <p className={formStyle.forgotPassword}>
-                  or create an <Link to='/auth/signup'>account</Link>
-        </p>
-      </Form>
-    )
-  }
+      <Button variant='primary' type='submit' className='btn btn-primary btn-block'>
+        Submit
+      </Button>
+      <p className={formStyle.forgotPassword}>
+        or create an <Link to='/auth/signup'>account</Link>
+      </p>
+    </Form>
+  )
 }
